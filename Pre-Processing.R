6a2f0c9c5ae752839782dd6a5f6bf1002072e163
sthirumalais <sthirumalais@student.unimelb.edu.au>
2025-10-22 10:50:36 +0530

    Fix: Pre-Processing

--- Pre-Processing.R
+++ Pre-Processing.R
@@ -5,8 +5,8 @@ sightings_data <- read.csv(
 )
 
 # Define column groups
-species <- c("scientificName", "vernacularName", "individualCount")
-taxonomy <- c("taxonConceptID", "species", "family", "genus", "order")
+species <- c("scientificName", "species", "vernacularName", "individualCount")
+taxonomy <- c("family", "genus", "order")
 geographic <- c("decimalLatitude", "decimalLongitude")
 temporal <- c("eventDate")
 
@@ -16,10 +16,46 @@ columns <- c(species, taxonomy, geographic, temporal)
 # Select only the required columns
 sightings_data <- sightings_data %>% select(all_of(columns))
 
+sightings_data %>%
+  distinct(scientificName) %>%
+  nrow()
+
 # Set missing individualCount to 1 (assume single bird sighting)
 sightings_data$individualCount[is.na(sightings_data$individualCount)] <- 1
 
+sightings_data <- sightings_data %>%
+  filter(
+    !grepl(
+      "Streptopelia|Lalage|AVES",
+      scientificName,
+      ignore.case = TRUE
+    )
+  )
+
+# For "Porzana" in scientific name, set species name and common name
+sightings_data <- sightings_data %>%
+  mutate(
+    species = ifelse(
+      grepl("Porzana", scientificName, ignore.case = TRUE),
+      "Porzana fluminea",
+      species
+    ),
+    vernacularName = ifelse(
+      grepl("Porzana", scientificName, ignore.case = TRUE),
+      "Australian Spotted Crake",
+      vernacularName
+    )
+  )
+
+sightings_data <- sightings_data %>% select(-scientificName)
+
+colnames(sightings_data)[
+  colnames(sightings_data) == "species"
+] <- "scientificName"
+
 # Save the processed data
-write.csv(sightings_data,
-          "Data/Pre - Processed Data/sightings_data.csv",
-          row.names = FALSE)
+write.csv(
+  sightings_data,
+  "Data/Pre - Processed Data/sightings_data.csv",
+  row.names = FALSE
+)
